ba2947867af9c6454ce451f88dda97de662aab95
hironori sasaki <hrnr.ssk@gmail.com>
2020-06-14 06:50:05 +0900

    ツイート確認後に一覧へ戻る機能を作成

--- app/helpers/bishlogs_helper.rb
+++ app/helpers/bishlogs_helper.rb
@@ -1,9 +1,9 @@
-module BlogsHelper
-    def choose_new_or_edit
-      if action_name == 'new' || action_name == 'create'
-        confirm_bishlogs_path
-      elsif action_name == 'edit'
-        bishlog_path
-      end
+module BishlogsHelper
+  def choose_new_or_edit
+    if action_name == 'new' || action_name == 'create'
+      confirm_bishlogs_path
+    elsif action_name == 'edit'
+      bishlog_path
     end
   end
+end
